bce87a403b8c4c3edf43429b04c6fd2eacb20698
GigiaJ <gigiaboone@yahoo.com>
2026-03-17 10:17:58 -0500

    tangled

--- src/client/core.cljs
+++ src/client/core.cljs
@@ -3,14 +3,13 @@
    [taoensso.timbre :as log]
    [utils.logger :as logger]
    [reagent.core :as r]
-            [reagent.dom.client :as rdom]
-            [client.login :as login]
-            [re-frame.core :as re-frame]
-            [service-worker-handler :refer [register-sw!]]
-            [client.state :as state :refer [sdk-world]]
-            [promesa.core :as p]
-            [app :as app]
-           )
+   [reagent.dom.client :as rdom]
+   [client.login :as login]
+   [re-frame.core :as re-frame]
+   [service-worker-handler :refer [register-sw!]]
+   [client.state :as state :refer [sdk-world]]
+   [promesa.core :as p]
+   [app :as app])
   (:require-macros [utils.macros :refer [ocall oget load-static-config]]))
 
 (re-frame/reg-event-db
@@ -25,7 +24,7 @@
 
 
 (defn ^:export init []
-(app/init)
-(logger/init!)
-(log/debug  "Entering Paradise!")
+  (app/init)
+  (logger/init!)
+  (log/debug  "Entering Paradise!")
   )
