4a58e51d7b03b8d8c58f3ad9a2e0235543e70ef5
GigiaJ <gigiaboone@yahoo.com>
2026-03-21 13:04:50 -0500

    tangled

--- src/client/config.cljs
+++ src/client/config.cljs
@@ -1,8 +1,17 @@
 (ns client.config
   (:require [cljs.reader :refer [read-string]]
+   [taoensso.timbre :as log]
             [promesa.core :as p]))
 
 (defn load-config []
-  (-> (js/fetch "/config.edn")
+  (-> (js/fetch "./config.edn"  #js {:cache "no-store"} )
       (p/then #(.text %))
       (p/then #(read-string %))))
+
+(defn check-remote-version []
+  (let [url (str "./config.edn?t=" (.getTime (js/Date.)))]
+    (-> (js/fetch url #js {:cache "no-store"})
+        (p/then #(.text %))
+        (p/then #(read-string %))
+        (p/then :version)
+        (p/catch (fn [_] nil)))))
