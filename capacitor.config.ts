48616b076a19bd843f2e624520d0d1087d0241c5
GigiaJ <gigiaboone@yahoo.com>
2026-03-29 16:50:51 -0500

    some more capacitor changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.gigiaj.paradise',
-  appName: 'Paradise',
-  webDir: 'dist'
+    appId: "com.gigiaj.paradise",
+    appName: "Paradise",
+    webDir: "dist",
+    plugins: {
+        SplashScreen: {
+            launchShowDuration: 3000,
+            launchAutoHide: false,
+            backgroundColor: "#313338",
+            showSpinner: true,
+            androidSpinnerStyle: "large",
+            iosSpinnerStyle: "small",
+            spinnerColor: "#dbdee1",
+        },
+    },
 };
 
 export default config;
