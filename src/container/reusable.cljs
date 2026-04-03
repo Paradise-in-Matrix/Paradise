7a2bcdba1fae1e329f9e1dd41ca8a4d78496ab94
GigiaJ <gigiaboone@yahoo.com>
2026-04-03 02:56:42 -0500

    tangled

--- src/container/reusable.cljs
+++ src/container/reusable.cljs
@@ -1,14 +1,9 @@
 (ns container.reusable
-            (:require [re-frame.core :as re-frame]
-            [taoensso.timbre :as log]
-            [re-frame.db :as db]
-            ["react-virtuoso" :refer [Virtuoso]]
+  (:require [re-frame.core :as re-frame]
             [container.timeline.item :refer [event-tile]]
-            [promesa.core :as p]
-            [reagent.core :as r]
-            [reagent.dom.client :as rdom]
-            [utils.svg :as icons]
-            ))
+            [utils.svg :as icons]))
+
+
 
 (re-frame/reg-event-db
  :container/set-main-focus
@@ -35,30 +30,30 @@
         main-focus    @(re-frame/subscribe [:container/main-focus])
         side-panel    @(re-frame/subscribe [:container/side-panel])
         call-active?  @(re-frame/subscribe [:call/is-active?])
+        room-meta     @(re-frame/subscribe [:rooms/active-metadata])
+        final-name    (or (:name room-meta) display-name active-id)
         in-lobby?     (and (= main-focus :timeline) call-active?)
         show-actions? (or (not compact?) (= main-focus :call))]
     [:div.room-header
      [:div.header-left
       [:button.mobile-menu-btn {:on-click #(re-frame/dispatch [:ui/toggle-sidebar])}
-       [icons/arrow-left]
-       ]
-      (when display-name
-        [:h2.room-header-name display-name])]
+       [icons/arrow-left]]
+      (when final-name
+        [:h2.room-header-name final-name])]
      (when (and show-actions? active-id)
        [:div.header-actions
         [:button.header-icon-btn
          {:title
           (if in-lobby?
-                   (tr [:container.header/join-call])
-                   (tr [:container.header/start-call]))
+            (tr [:container.header/join-call])
+            (tr [:container.header/start-call]))
           :class (when (or (= main-focus :call) call-active?) "active-green")
           :on-click (fn [_]
                       (re-frame/dispatch [:call/init-widget active-id])
                       (re-frame/dispatch [:container/set-main-focus :call]))}
-         [icons/phone]
-         ]
+         [icons/phone]]
 
-       (when (= main-focus :call)
+        (when (= main-focus :call)
           [:button.header-icon-btn
            {:title (tr [:container.header/room-chat])
             :class (when (= side-panel :timeline) "active")
@@ -85,22 +80,13 @@
           :on-click #(re-frame/dispatch [:container/set-side-panel :members])}
          [icons/members]]
 
-
         [:button.header-icon-btn
          {:title (tr [:container.header/more-options])
           :on-click (fn [e]
                       (let [mx (.-clientX e)
                             my (.-clientY e)]
-                        (re-frame/dispatch [:context-menu/open {:x mx :y my :items (build-room-actions active-id display-name)}])))}
+                        (re-frame/dispatch [:context-menu/open {:x mx :y my :items (build-room-actions active-id final-name)}])))}
          [icons/more-vertical]]])]))
-
-(defn make-timeline-item-shim [item event-id]
-  (log/error item)
-  #js {:uniqueId (fn []
-                   #js {:id event-id})
-       :asEvent  (fn []
-                   item)
-       :asVirtual (fn [] nil)})
 
 (defn message-preview-item [room-id event]
   (let [tr @(re-frame/subscribe [:i18n/tr])]
