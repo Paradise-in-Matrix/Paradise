1ff1731f7b044fd3a7059584bcc815689dcce44e
GigiaJ <gigiaboone@yahoo.com>
2026-04-03 02:57:17 -0500

    tangled

--- src/navigation/spaces/bar.cljs
+++ src/navigation/spaces/bar.cljs
@@ -1,178 +1,94 @@
 (ns navigation.spaces.bar
-  (:require [promesa.core :as p]
-            [re-frame.core :as re-frame]
-            [re-frame.db :as db]
-            [reagent.core :as r]
-            ["react-virtuoso" :refer [Virtuoso]]
-            [client.state :as state :refer [sdk-world]]
-            [navigation.rooms.room-list :refer [process-parent-queue!]]
-            [client.diff-handler :refer [apply-matrix-diffs apply-generic-diffs!]]
-            [overlays.settings :refer [sidebar-profile-mini]]
-            [utils.svg :as icons]
-            [utils.helpers :refer [mxc->url]]
-            [utils.global-ui :refer [avatar]]
-            [taoensso.timbre :as log]))
-
-(defn parse-space-child [obj]
-  (let [id   (or (.-roomId obj) (.-id obj))
-        name (or (.-displayName obj) (.-name obj) "Unknown")
-        type-tag (some-> obj .-roomType .-tag)
-        ]
-    (if-not id
-      nil
-      {:id        id
-       :name      name
-       :avatar-url (.-avatarUrl obj)
-       :is-space? (= type-tag "Space")
-       :raw-room  obj})))
-
-
-(defonce !global-space-mutex (atom (p/resolved nil)))
-
-(defn apply-global-spaces-diffs! [updates]
-  (apply-generic-diffs!
-   {:!mutex      !global-space-mutex
-    :db-path     [:spaces-list]
-    :parse-fn
-    #(parse-space-child %)
-    :sync-event  [:sdk/set-spaces-list-sync]
-    :async-event [:sdk/process-spaces]
-    :updates     updates}))
-
-(defonce !space-mutexes (atom {}))
-
-(defn apply-space-rooms-diffs! [space-id updates]
-  (swap! !space-mutexes update space-id #(or % (atom (p/resolved nil))))
-  (apply-generic-diffs!
-   {:!mutex      (get @!space-mutexes space-id)
-    :db-path     [:space-rooms space-id]
-    :parse-fn    #(parse-space-child %)
-    :sync-event  [:sdk/update-space-view space-id]
-    :async-event nil
-    :updates     updates}))
-
-
-(defn init-space-service! [client]
-  (p/let [space-service (.spaceService client)
-          _ (re-frame/dispatch [:sdk/set-space-service space-service])
-          listener #js {:onUpdate #(apply-global-spaces-diffs! %)}
-          sub (.subscribeToJoinedSpaces space-service listener)
-          initial-spaces (.joinedSpaces space-service)
-          _ (js/console.log initial-spaces)
-          _ (re-frame/dispatch [:sdk/set-global-space-sub sub])
-          initial-spaces (.joinedSpaces space-service)]
-    (process-parent-queue!)))
-
-
-(re-frame/reg-event-db
- :sdk/save-space-sub
- (fn [db [_ space-id space-list sub-handle]]
-   (assoc-in db [:space-subs space-id] {:list space-list :sub sub-handle})))
-
-
-(re-frame/reg-event-db
- :sdk/set-global-space-sub
- (fn [db [_ sub-handle]]
-   (assoc db :global-space-sub sub-handle)))
+  (:require
+   [re-frame.core :as re-frame]
+   [reagent.core :as r]
+   ["react-virtuoso" :refer [Virtuoso]]
+   [cljs-workers.core :as main]
+   [client.state :as state]
+   [cljs.core.async :refer [go <!]]
+   [overlays.settings :refer [sidebar-profile-mini]]
+   [utils.svg :as icons]
+   [utils.helpers :refer [mxc->url]]
+   [utils.global-ui :refer [avatar]]))
 
 (re-frame/reg-event-db
  :sdk/set-spaces-list-sync
  (fn [db [_ spaces-vec]]
-   (assoc db :spaces-list spaces-vec)))
+   (let [spaces-map (into {} (for [s spaces-vec :when (:id s)] [(:id s) s]))]
+     (-> db
+         (assoc :spaces-list spaces-vec)
+         (assoc :spaces spaces-map)))))
 
-
-(re-frame/reg-event-fx
- :sdk/process-spaces
- (fn [{:keys [db]} [_ parsed-spaces-array]]
-   (let [new-spaces-map (into {} (for [s parsed-spaces-array
-                                       :when (:id s)]
-                                   [(:id s) s]))
-         existing-subs (or (:space-subs db) {})
-         space-service (:space-service db)
-         new-space-ids (remove #(contains? existing-subs %) (keys new-spaces-map))]
-     {:db (assoc db :spaces new-spaces-map)
-      :dispatch-n (for [id new-space-ids]
-                    [:sdk/boot-background-space-list space-service id])
-      })))
-
-(re-frame/reg-event-fx
- :sdk/boot-background-space-list
- (fn [_ [_ service space-id]]
-   (-> (.spaceRoomList service space-id)
-       (.then (fn [space-list]
-                (let [listener #js {:onUpdate (fn [diffs] (apply-space-rooms-diffs! space-id diffs))}]
-                  (try
-                    (p/let [sub-handle (.subscribeToRoomUpdate space-list listener)
-                            _ (re-frame/dispatch [:sdk/save-space-sub space-list sub-handle])
-                            _ (.paginate space-list)
-                            
-                            ]
-                      ;;(apply-space-rooms-diffs! space-id #js {:tag "Reset" :inner initial-rooms})
-                      )
-                    (catch :default e
-                       (js/console.error "FFI Subscription Panic:" e))))))
-       (.catch (fn [err]
-                 (js/console.error "Failed to boot space list for" space-id ":" err))))
-   {}))
+(re-frame/reg-event-db
+ :sdk/update-space-view
+ (fn [db [_ space-id space-items]]
+   (let [children-refs (mapv (fn [item]
+                               {:id        (:id item)
+                                :is-space? (:is-space? item)})
+                             space-items)]
+     (assoc-in db [:space-children space-id] children-refs))))
 
 (re-frame/reg-event-fx
  :sdk/paginate-space
- (fn [{:keys [db]} [_ space-id]]
-   (let [space-sub (get-in db [:space-subs space-id])
-         space-list (:list space-sub)]
-     (when space-list
-       (.paginate space-list))
-     {})))
-
-(re-frame/reg-event-fx
- :room-list/paginate-global
- (fn [{:keys [db]} _]
-   (when-let [controller (:room-list-controller db)]
-     (.addOnePage controller))
+ (fn [_ [_ space-id]]
+   (main/do-with-pool! @state/!engine-pool {:handler :paginate-space
+                                            :arguments {:space-id space-id}})
    {}))
 
-(re-frame/reg-event-fx
- :sdk/paginate-space-fully
- (fn [{:keys [db]} [_ space-id]]
-   (let [space-sub  (get-in db [:space-subs space-id])
-         space-list (:list space-sub)]
-     (when space-list
-       (-> (.paginate space-list)
-           (.then (fn [has-more?]
-                    (log/debug "Pagination returned:" has-more?)
-                    (if has-more?
-                      (re-frame/dispatch [:sdk/paginate-space-fully space-id])
-                      (log/debug "Fully loaded all rooms for space:" space-id))))
-           (.catch (fn [err]
-                     (log/error "Pagination panic for space:" space-id err)))))
-     {})))
-
-(defn fetch-space-hierarchy [space-id]
-  (let [client @(re-frame/subscribe [:sdk/client])
-        base-url (.homeserver client)
-        token (.-accessToken (.session client))
-        url (str base-url "_matrix/client/v1/rooms/" space-id "/hierarchy")]
-    (-> (js/fetch url #js {:headers #js {:Authorization (str "Bearer " token)}})
-        (p/then #(.json %))
-        (p/then (fn [json]
-                  (let [resp (js->clj json :keywordize-keys true)]
-                    (re-frame/dispatch [:rooms/process-hierarchy space-id (:rooms resp)])))))))
+(re-frame/reg-event-db
+ :space/process-hierarchy
+ (fn [db [_ root-space-id rooms]]
+   (let [hierarchy-orders
+         (reduce
+          (fn [acc room]
+            (let [parent-id       (:room_id room)
+                  children-events (:children_state room)]
+              (if (seq children-events)
+                (let [child-map (reduce
+                                 (fn [cmap event]
+                                   (if (= (:type event) "m.space.child")
+                                     (let [child-id (:state_key event)
+                                           order    (get-in event [:content :order] "zzzz")]
+                                       (assoc cmap child-id order))
+                                     cmap))
+                                 {} children-events)]
+                  (assoc acc parent-id child-map))
+                acc)))
+          {} rooms)]
+     (update db :space-orders merge hierarchy-orders))))
 
 (re-frame/reg-event-fx
  :sdk/fetch-space-hierarchy
  (fn [_ [_ space-id]]
-   (fetch-space-hierarchy space-id)
+   (go
+     (let [pool @state/!engine-pool
+           res  (<! (main/do-with-pool! pool {:handler :fetch-space-hierarchy
+                                              :arguments {:space-id space-id}}))]
+       (when (= (:status res) "success")
+         (re-frame/dispatch [:space/process-hierarchy space-id (:rooms res)]))))
    {}))
 
-(re-frame/reg-sub
- :spaces/all
- (fn [db _]
-   (get db :spaces-list [])))
+(re-frame/reg-event-fx
+ :space/select
+ (fn [{:keys [db]} [_ space-id]]
+   (let [current-space (:active-space-id db)]
+     (if (= current-space space-id)
+       {}
+       {:db (-> db
+                (assoc :active-space-id space-id)
+                (assoc :active-room-id nil))
+        :dispatch-n (if space-id
+                      [[:sdk/paginate-space space-id]
+                       [:sdk/fetch-room-emotes :space space-id]
+                       [:sdk/fetch-space-hierarchy space-id]
+                       [:room-list/apply-filter "all"]]
+                      [[:room-list/apply-filter "people"]])}))))
 
-(re-frame/reg-sub
- :spaces/active-id
- (fn [db _]
-   (:active-space-id db)))
+
+
+(re-frame/reg-sub :spaces/all (fn [db _] (get db :spaces-list [])))
+(re-frame/reg-sub :spaces/active-id (fn [db _] (:active-space-id db)))
+(re-frame/reg-sub :space-children/map (fn [db _] (:space-children db)))
 
 (re-frame/reg-sub
  :spaces/active-metadata
@@ -183,24 +99,8 @@
      (some #(when (= (:id %) active-id) %) all-spaces))))
 
 (re-frame/reg-sub
- :space-children/map
- (fn [db _]
-   (:space-children db)))
-
-
-(re-frame/reg-sub
- :space-rooms-map
- (fn [db _]
-   (or (:space-rooms db) {})))
-
-(re-frame/reg-sub
- :sdk/space-service
- (fn [db _]
-   (:space-service db)))
-
-(re-frame/reg-sub
  :space/children
- :<- [:space-children]
+ :<- [:space-children/map]
  :<- [:rooms/all]
  (fn [[rel-map all-rooms] [_ parent-id]]
    (let [child-refs (get rel-map parent-id [])
@@ -211,67 +111,45 @@
                       rich-room)))
            child-refs))))
 
-(re-frame/reg-event-db
- :sdk/update-space-view
- (fn [db [_ space-id space-items]]
-   (let [children-refs (mapv (fn [item]
-                               {:id        (or (:id item) (.-roomId item) (.-id item))
-                                :is-space? (:is-space? item)})
-                             space-items)
-         _ (log/debug children-refs)
-         ]
-     (assoc-in db [:space-children space-id] children-refs))))
-
-(re-frame/reg-event-db
- :sdk/set-space-service
- (fn [db [_ service]]
-   (assoc db :space-service service)))
-
-(re-frame/reg-event-fx
- :space/select
- (fn [{:keys [db]} [_ space-id]]
-   (let [current-space (:active-space-id db)]
-     (if (= current-space space-id)
-       (do (process-parent-queue!)
-       {})
-       {:db (-> db
-                (assoc :active-space-id space-id)
-                (assoc :active-room-id nil))
-        :dispatch-n (if space-id
-                      [[:sdk/paginate-space space-id]
-                       [:sdk/fetch-room-emotes :space space-id]
-                       [:sdk/fetch-space-hierarchy space-id]]
-                      [[:room-list/apply-filter "people"]])}))))
-
 (defn space-icon-item [item active-id]
   (let [{:keys [id name avatar-url is-dm?]} item
-        stats (if is-dm?
-                item
-                @(re-frame/subscribe [:space/unread-stats id]))
+        stats (if is-dm? item @(re-frame/subscribe [:space/unread-stats id]))
+        {:keys [unread? highlight? mentions msg-count]} stats
+        badge-count (if is-dm? msg-count mentions)
 
-        {:keys [unread? highlight? mentions]} stats]
+        profile          @(re-frame/subscribe [:sdk/profile])
+        my-id            (:user-id profile)
+        needs-members?   is-dm?
+        members-map      (when is-dm? @(re-frame/subscribe [:room/members-map id]))
+        members-loading? (when is-dm? @(re-frame/subscribe [:room/members-loading? id]))]
 
-    [:div.space-icon-wrapper
-     {:class [(when (= id active-id) "active")
-              (when unread? "has-unread")
-              (when highlight? "has-highlight")
-              (when is-dm? "is-dm-entry")]
-      :on-click #(if is-dm?
-                   (do
-                     (re-frame/dispatch [:space/select nil])
-                     (re-frame/dispatch [:rooms/select id]))
-                   (re-frame/dispatch [:space/select id]))
-      :title name}
+    (when (and needs-members? (not members-loading?) (empty? members-map))
+      (re-frame/dispatch [:room/fetch-members id]))
 
-     (when unread?
-       [:div.space-notification-pill])
+    (let [other-user   (when (and is-dm? (seq members-map))
+                         (->> (vals members-map)
+                              (remove #(= (:user-id %) my-id))
+                              first))
+          final-avatar (or (mxc->url avatar-url) (:avatar-url other-user))
+          final-name   (if (and is-dm? other-user (= name "Loading..."))
+                         (:display-name other-user)
+                         name)]
 
-     [avatar {:id id :name name :url (mxc->url avatar-url) :size 42 :shape :squircle}]
-
-     (when (and mentions (pos? mentions))
-       [:div.space-mention-badge mentions])]))
-
-
+      [:div.space-icon-wrapper
+       {:class [(when (= id active-id) "active")
+                (when unread? "has-unread")
+                (when highlight? "has-highlight")
+                (when is-dm? "is-dm-entry")]
+        :on-click #(if is-dm?
+                     (do
+                       (re-frame/dispatch [:space/select nil])
+                       (re-frame/dispatch [:rooms/select id]))
+                     (re-frame/dispatch [:space/select id]))
+        :title final-name}
+       (when unread? [:div.space-notification-pill])
+       [avatar {:id id :name final-name :url final-avatar :size 42 :shape :squircle}]
+       (when (and badge-count (pos? badge-count))
+         [:div.space-mention-badge badge-count])])))
 
 (defn virtualized-space-bar [spaces active-space-id]
   (let [space-array (to-array spaces)]
@@ -284,12 +162,14 @@
          ^{:key (:id space-map)}
          [space-icon-item space-map active-space-id]))}]))
 
+
+
+
 (defn spaces-sidebar []
   (let [spaces        @(re-frame/subscribe [:spaces/all])
         active-id     @(re-frame/subscribe [:spaces/active-id])
         active-dms    @(re-frame/subscribe [:rooms/active-dm-pops])
-        tr            @(re-frame/subscribe [:i18n/tr])
-        ]
+        tr            @(re-frame/subscribe [:i18n/tr])]
     [:div.sidebar-spaces
      [:div.home-group
       [:div.space-icon-wrapper
