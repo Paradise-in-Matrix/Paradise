7ded5493a768bacd7dfb59bf027d84f1cb58d9dd
GigiaJ <gigiaboone@yahoo.com>
2026-04-05 07:12:36 -0500

    tangled

--- src/input/emotes.cljs
+++ src/input/emotes.cljs
@@ -4,6 +4,9 @@
    [re-frame.core :as re-frame]
    [taoensso.timbre :as log]
    [clojure.string :as str]
+   [cljs.core.async :refer [go <!]]
+   [cljs-workers.core :as main]
+   [client.state :as state]
    [reagent.core :as r]
    [utils.helpers :refer [mxc->url fetch-state-event fetch-room-state]]
    [utils.global-ui :refer [click-away-wrapper]]
@@ -11,66 +14,30 @@
    ["react" :as react]
    ))
 
-(defn shape-emote-packs [room-id events]
-  (->> events
-       (filter #(= (:type %) "im.ponies.room_emotes"))
-       (map (fn [e] {:pack-id (or (not-empty (:state_key e)) room-id)
-                     :data (:content e)}))))
-
-(defn unwrap-rooms [raw-json]
-  (let [data (js->clj (js/JSON.parse raw-json) :keywordize-keys true)
-        rooms-map (:rooms data)]
-    (for [[rid packs] rooms-map
-          [pack-name _] packs]
-      [(name rid) (name pack-name)])))
-
-(defn fetch-emote-packs [homeserver token [rid pack-name]]
-  (-> (fetch-state-event homeserver token rid "im.ponies.room_emotes" pack-name)
-      (p/then (fn [json]
-                (when json
-                  {:pack-id pack-name :data json})))
-      (p/catch (fn [err] (log/error "Failed to fetch pack" pack-name err) nil))))
-
-(re-frame/reg-fx
- :principle-fetch-emotes
- (fn [{:keys [token homeserver client]}]
-   (p/let [raw-json (.accountData client "im.ponies.emote_rooms")
-           fetch-targets (unwrap-rooms raw-json)
-           results (p/all (map #(fetch-emote-packs homeserver token %) fetch-targets))]
-     (re-frame/dispatch [:sdk/save-all-emotes :account nil (remove nil? results)]))))
-
-(re-frame/reg-fx
- :principle-fetch-room-emotes-fx
- (fn [{:keys [token homeserver source-type room-id]}]
-   (-> (fetch-room-state homeserver token room-id nil nil
-                         (partial shape-emote-packs room-id))
-       (p/then (fn [packs]
-                 (when (seq packs)
-                   (re-frame/dispatch [:sdk/save-all-emotes source-type room-id packs]))))
-       (p/catch #(log/error "Room fetch failed" room-id %)))))
+(re-frame/reg-event-fx
+ :sdk/fetch-room-emotes
+ (fn [_ [_ source-type room-id]]
+   (if-let [pool @state/!engine-pool]
+     (go
+       (let [res (<! (main/do-with-pool! pool
+                                         {:handler :fetch-emotes
+                                          :arguments {:source-type (name source-type)
+                                                      :room-id room-id}}))]
+         (if (= (:status res) "success")
+           (re-frame/dispatch [:sdk/save-all-emotes source-type room-id (:packs res)])
+           (log/error "Worker emote fetch failed:" (:msg res)))))
+     (log/error "No engine pool to fetch emotes"))
+   {}))
 
 (re-frame/reg-event-fx
  :sdk/fetch-all-emotes
  (fn [{:keys [db]} _]
-   (let [client (:client db)
-         session (some-> client (.session))]
-     (if (and client session)
-       {:principle-fetch-emotes {:token (.-accessToken session)
-                                 :homeserver (.-homeserverUrl session)
-                                 :client client}}
-       {}))))
-
-(re-frame/reg-event-fx
- :sdk/fetch-room-emotes
- (fn [{:keys [db]} [_ source-type room-id]]
-   (let [client (:client db)
-         session (some-> client (.session))]
-     (if (and session room-id)
-       {:principle-fetch-room-emotes-fx {:token (.-accessToken session)
-                                         :homeserver (.-homeserverUrl session)
-                                         :source-type source-type
-                                         :room-id room-id}}
-       {}))))
+   (re-frame/dispatch [:sdk/fetch-room-emotes :account nil])
+   (when-let [active-room (:active-room-id db)]
+     (re-frame/dispatch [:sdk/fetch-room-emotes :room active-room]))
+   (when-let [active-space (:active-space-id db)]
+     (re-frame/dispatch [:sdk/fetch-room-emotes :space active-space]))
+   {}))
 
 (re-frame/reg-event-db
  :sdk/save-all-emotes
