b829457a5cb68dd70b28cbde8b471979cc7ddb46
GigiaJ <gigiaboone@yahoo.com>
2026-04-16 11:27:02 -0500

    tangled

--- src/container/timeline/virtualizer.cljs
+++ src/container/timeline/virtualizer.cljs
@@ -3,113 +3,71 @@
             [re-frame.core :as re-frame]
             [clojure.string :as str]
             [taoensso.timbre :as log]
+            [container.timeline.item :as item]
             ["@chenglou/pretext" :refer [prepare layout]]
             [container.timeline.item :refer [connected-event-tile]]))
 
-(defn timeline-empty-state []
+(defn timeline-empty-state [room-id]
   (let [tr @(re-frame/subscribe [:i18n/tr])]
     [:div.timeline-empty {:style {:padding "40px" :text-align "center"}}
-     (tr [:container.timeline/loading])]))
+     (if room-id
+         (tr [:container.timeline/loading])
+         (tr [:container.timeline/no-room])
+         )]))
+
 
 (defn timeline-jump-button [do-jump! focus-mode?]
   (let [tr @(re-frame/subscribe [:i18n/tr])]
     [:button.jump-to-bottom
-     {:style {:position "absolute"
-              :bottom "20px"
-              :right "24px"
-              :z-index 10
-              :display "flex"
-              :align-items "center"
-              :gap "8px"}
-      :on-click do-jump!}
+     {:on-click do-jump!}
      [:span (if focus-mode?
               (tr [:container.timeline/return-to-live])
               (tr [:container.timeline/jump-to-bottom]))]]))
+
+(defn timeline-loading-overlay []
+  [:div.timeline-loading-overlay
+   [:div.timeline-loading-spinner-container
+    [:div.spinner]]])
 
 (defn get-computed-metrics [el]
   (let [style (js/window.getComputedStyle el)
         font-size (.getPropertyValue style "font-size")
         font-family (.getPropertyValue style "font-family")
-        line-height (.getPropertyValue style "line-height")]
-    {:font (str font-size " " font-family)
-     :line-height (if (= line-height "normal")
-                    (* (js/parseFloat font-size) 1.2)
-                    (js/parseFloat line-height))}))
+        line-height (.getPropertyValue style "line-height")
+        get-var (fn [var-name fallback]
+                  (let [val (str/trim (.getPropertyValue style var-name))]
+                    (if (empty? val) fallback (js/parseFloat val))))
+        get-str-var (fn [var-name fallback]
+                      (let [val (str/trim (.getPropertyValue style var-name))]
+                        (if (empty? val) fallback val)))
+        avatar-size (get-var "--chat-avatar-size" 32)
+        avatar-gap  (get-var "--chat-avatar-gap" 14)]
+    {:font             (get-str-var "--chat-body-font" "15.2px sans-serif")
+     :line-height      (get-var "--chat-body-line-height" 22.8)
+
+     :avatar-h          avatar-size
+     :avatar-col-w      (+ avatar-size avatar-gap)
+     :header-h          (get-var "--chat-header-h" 26.8)
+     :message-padding-x (get-var "--chat-padding-x" 40)
+     :seq-padding       (get-var "--chat-seq-padding" 10)
+     :merged-padding    (get-var "--chat-merged-padding" 0)
+     :media-margin      (get-var "--chat-media-margin" 0)
+     :reaction-row-h    (get-var "--chat-reaction-row-h" 24)
+     :edited-label-h    (get-var "--chat-edited-label-h" 16)
+     :reply-banner-h    (get-var "--chat-reply-banner-h" 32.4)
+     :text-wrap-buffer  (get-var "--chat-text-wrap-buffer" 4)
+     :quote-wrap-buffer (get-var "--chat-quote-wrap-buffer" 19)
+     :url-wrap-buffer   (get-var "--chat-url-wrap-buffer" 40)
+     :code-padding      (get-var "--chat-code-padding" 28)
+     :code-font         (get-str-var "--chat-code-font" "13.68px 'fira code', monospace")
+     :code-line-height  (get-var "--chat-code-line-height" 20.52)}))
 
 (defn estimate-height [msg width pretext-cache measured-heights theme-metrics]
   (let [id (:id msg)
         cached-measured (get measured-heights id)]
     (if cached-measured
       cached-measured
-      (let [type (:type msg)
-            content-tag (:content-tag msg)]
-        (cond
-          (or (= type :virtual) (= type "virtual") (str/starts-with? (str id) "virtual-divider")) 49
-          (and content-tag (not (#{"MsgLike" "Image" "Video" "Sticker" "File" "Text"} content-tag))) 53
-          :else
-          (let [is-sequence-start? (not (:merge-with-prev? msg))
-                avatar-h 32
-                avatar-col-w 46
-                header-h 26.8
-                seq-padding 10
-                merged-padding 0
-                message-padding-x 40
-                media-margin 4
-                reaction-h (if (seq (:reactions msg)) 24 0)
-
-                content (:content msg)
-                info (or (:info content) (get-in content [:inner :content :info]))
-                w (js/parseFloat (:w info 0))
-                h (js/parseFloat (:h info 0))
-                available-w (max 0 (- width avatar-col-w message-padding-x))
-                actual-tag (or (get-in content [:inner :tag]) content-tag)
-                is-media? (#{"Image" "Video" "Sticker"} actual-tag)
-
-                final-height
-                (if is-media?
-                  (let [valid-dims? (and (pos? w) (pos? h))
-                        _ (log/warn valid-dims?)
-                        default-ratio (case actual-tag "Video" 1.77 "Sticker" 1.0 1.33)
-                        calc-w (if valid-dims? (min w available-w 400) 300)
-                        calc-h (if valid-dims? (min (* calc-w (/ h w)) 350) (min (/ calc-w default-ratio) 350))
-                        media-h (+ calc-h media-margin)]
-                    (if is-sequence-start?
-                      (+ (max avatar-h (+ header-h media-h)) seq-padding reaction-h)
-                      (max avatar-h (+ media-h merged-padding reaction-h))))
-                  (let [raw-txt (or (:body msg) (:body content) (get-in content [:inner :content :body]) "")
-                        txt-str (if (string? raw-txt) raw-txt (str raw-txt))
-                        html-txt (or (get-in content [:inner :content :html]) "")
-                        has-code-block? (or (clojure.string/includes? txt-str "```")
-                                            (clojure.string/includes? html-txt "<pre>"))
-                        font-str (if has-code-block?
-                                   "13.68px 'fira code', monospace"
-                                   (:font theme-metrics "16px sans-serif"))
-                        lh (if has-code-block?
-                             20.52
-                             (:line-height theme-metrics 22.8))
-                        code-padding (if has-code-block? 28 0)
-
-                        cache-key (str id "-" available-w "-" has-code-block?)
-                        cached-text-h (get @pretext-cache cache-key)
-                        final-text-h
-                        (or cached-text-h
-                            (let [lines (clojure.string/split txt-str #"\n")
-                                  total-h (reduce (fn [acc line]
-                                                    (if (empty? (clojure.string/trim line))
-                                                      (+ acc lh)
-                                                      (let [prep (prepare line font-str)
-                                                            raw (try (layout prep available-w lh) (catch js/Error _ nil))]
-                                                        (+ acc (if raw (.-height raw) lh)))))
-                                                  0
-                                                  lines)]
-                              (swap! pretext-cache assoc cache-key total-h)
-                              total-h))]
-
-                    (if is-sequence-start?
-                      (+ (max avatar-h (+ header-h final-text-h code-padding)) seq-padding reaction-h)
-                      (max avatar-h (+ final-text-h code-padding merged-padding reaction-h)))))]
-            (log/info "Calculated height for ID:" id "->" final-height "| Media?:" is-media?)
-            final-height))))))
+      (item/calc-item-height msg width pretext-cache theme-metrics))))
 
 (defn calculate-layout [events width pretext-cache measured-heights theme-metrics]
   (reduce (fn [acc msg]
@@ -121,8 +79,7 @@
           {:total 0 :items []}
           events))
 
-(defn test-timeline [room-id]
-  (log/info "Outer re-render")
+(defn pretext-timeline [room-id]
   (r/with-let [!scroll-ref      (atom nil)
                !scroll-top      (r/atom 0)
                !container-width (r/atom 400)
@@ -141,7 +98,6 @@
                !show-jump?      (r/atom false)
                !initialized?    (r/atom false)
                !scroll-timer    (atom nil)
-
                item-resize-obs  (js/ResizeObserver.
                                  (fn [entries]
                                    (doseq [entry entries]
@@ -163,13 +119,10 @@
                                    (let [rect (.-contentRect (aget entries 0))]
                                      (reset! !container-width (.-width rect)))))]
     (fn [room-id]
-      (log/info "Rendered")
       (let [events           @(re-frame/subscribe [:timeline/current-events room-id])
-            events-map       @(re-frame/subscribe [:timeline/events-map room-id])
-            
+             events-map       @(re-frame/subscribe [:timeline/events-map room-id])
             _                (reset! !latest-events events-map)
             ordered-events   (keep #(or (get events-map (:id %)) %) (reverse events))
-            
             cnt              (count ordered-events)
             loading?         @(re-frame/subscribe [:timeline/loading-more? room-id])
             loading-forward? @(re-frame/subscribe [:timeline/loading-forward? room-id])
@@ -221,18 +174,11 @@
                  (reset! !at-bottom? (not jump-target)))))
            0))
 
-        [:div.virtua-timeline-wrapper
+        [:div.timeline-wrapper
          {:ref (fn [el]
                  (if el
                    (.observe container-obs el)
-                   (.disconnect container-obs)))
-          :style {:flex 1
-                  :position "relative"
-                  :display "flex"
-                  :flex-direction "column"
-                  :min-height 0
-                  :opacity (if (or @!initialized? (zero? cnt)) 1 0)
-                  :transition "opacity 0.15s ease-in-out"}}
+                   (.disconnect container-obs)))}
 
          [:div.timeline-item {:style {:position "absolute" :visibility "hidden" :pointer-events "none" :z-index -1}}
           [:div.timeline-body {:ref #(when % (.observe metrics-obs %))}
@@ -241,12 +187,6 @@
          [:div.timeline-messages
           {:ref #(reset! !scroll-ref %)
            :class (when jump-target "jumping-animation")
-           :style {:display "flex"
-                   :flex-direction "column-reverse"
-                   :flex 1
-                   :height "100%"
-                   :overflow-y "auto"
-                   :position "relative"}
            :on-scroll (fn [e]
                         (let [target           (.-currentTarget e)
                               scroll-top       (js/Math.round (.-scrollTop target))
@@ -281,21 +221,11 @@
                                        10))))))}
 
           (when loading?
-            [:div {:style {:position "absolute"
-                           :top "20px"
-                           :width "100%"
-                           :display "flex"
-                           :justify-content "center"
-                           :z-index 100
-                           :pointer-events "none"}}
-             [:div {:style {:background "var(--bg-color, white)"
-                            :padding "8px"
-                            :border-radius "50%"
-                            :box-shadow "0 2px 10px rgba(0,0,0,0.1)"}}
-              [:div.spinner]]])
+            [timeline-loading-overlay])
+
 
           (if (zero? cnt)
-            [timeline-empty-state]
+            [timeline-empty-state room-id]
 
             (let [first-vis  (first visible-window)
                   last-vis   (last visible-window)
@@ -304,24 +234,24 @@
               (into [:<>]
                     (concat
                      [^{:key "bottom-gap"}
-                      [:div.virtua-bottom-gap
+                      [:div.timeline-bottom-gap
                        {:style {:height (str bottom-gap "px")
                                 :flex-shrink 0
                                 :width "100%"}}]]
                      (for [{:keys [id height] :as item} visible-window]
                        ^{:key id}
-                       [:div.virtua-item-wrapper
+                       [:div.item-wrapper
                         {:style {:min-height (str height "px")
                                  :width "100%"
                                  :margin "0"
                                  :box-sizing "border-box"}}
+
                         [:div.timeline-item
                          {:data-event-id id
                           :ref (fn [el]
                                  (when el
                                    (.observe item-resize-obs el)))
-                          :class (when (= id jump-target) "is-jump-target")
-                          :style {:margin "0"}}
+                          :class (when (= id jump-target) "is-jump-target")}
                          [connected-event-tile room-id item]]])
                      [^{:key "top-gap"}
                       [:div.virtua-top-gap
@@ -330,11 +260,7 @@
                                 :width "100%"}}]]))))
 
           (when (and focus-mode? loading-forward?)
-            [:div {:style {:min-height "60px"
-                           :display "flex"
-                           :align-items "center"
-                           :justify-content "center"
-                           :padding-bottom "20px"}}
+            [:div.spinner-wrapper
              [:div.spinner]])]
 
          (when (and @!show-jump? (not @!at-bottom?))
