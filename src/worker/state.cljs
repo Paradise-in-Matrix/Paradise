7a92d291e2357c5263ca70c6b563f8b4d26b6002
GigiaJ <gigiaboone@yahoo.com>
2026-04-21 16:36:35 -0500

    tangled

--- src/worker/state.cljs
+++ src/worker/state.cljs
@@ -2,3 +2,6 @@
 
 (defonce !client (atom nil))
 (defonce !media-cache (atom {}))
+(defonce !plugin-handlers (atom {}))
+(defn register-handler [id handler-fn]
+  (swap! !plugin-handlers assoc id handler-fn))
