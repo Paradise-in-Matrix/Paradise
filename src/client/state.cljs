a9eabcd2d4e7f6b3a6d263570017ab68b1b523eb
GigiaJ <gigiaboone@yahoo.com>
2026-04-21 16:36:04 -0500

    tangled

--- src/client/state.cljs
+++ src/client/state.cljs
@@ -1,4 +1,15 @@
-(ns client.state)
+(ns client.state
+  (:require [reagent.core :as r]))
 
 (defonce !engine-pool (atom nil))
 (defonce !config (atom nil))
+(defonce !components (r/atom {}))
+(defonce !slots (r/atom {}))
+
+(defn reg-slot-item [slot-id component]
+  (swap! !slots update slot-id (fnil conj []) component))
+
+(defn get-slot [slot-id]
+  (get @!slots slot-id []))
+
+(set! (.-dumpRegistry js/window) #(clj->js @!components))
