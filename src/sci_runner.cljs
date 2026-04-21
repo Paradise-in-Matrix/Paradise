68b8a9c36b9cce649c6a4f32ee970ad5e951b82b
GigiaJ <gigiaboone@yahoo.com>
2026-04-21 16:26:40 -0500

    tangled

---
+++ src/sci_runner.cljs
@@ -0,0 +1,37 @@
+(ns sci-runner
+  (:require
+   [sci.core :as sci]
+   [re-frame.core :as rf]
+   [reagent.core :as r]
+   [client.state :as state]))
+
+
+
+
+(def defoverride-macro
+  (sci/new-macro-var
+   'defoverride
+   (fn [_form _env comp-name args & body]
+     (list 'do
+           (list* 'defn comp-name args body)
+           (list 'js/console.log "PLUGIN OVERWRITING REGISTRY FOR:" (keyword comp-name))
+           (list 'swap! 'client.state/!components 'assoc (keyword comp-name) comp-name)))))
+
+(def plugin-context
+  (sci/init
+   {:namespaces
+    {'user {'defoverride defoverride-macro}
+     're-frame.core {'dispatch rf/dispatch
+                     'subscribe rf/subscribe}
+     'reagent.core  {'as-element r/as-element}
+     'client.state  {'!components state/!components
+                     'reg-slot-item state/reg-slot-item
+                     'get-slot state/get-slot
+                     '!config state/!config}}
+    :classes {'js goog/global}}))
+
+
+(defn evaluate-ui-form [form-str]
+  (js/console.log "SCI RECEIVED THIS STRING TO EVALUATE:" form-str)
+  (sci/eval-string form-str plugin-context))
+
