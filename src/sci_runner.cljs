5ac0852b58ad6b8475b072f8565d7ec8d75383eb
GigiaJ <gigiaboone@yahoo.com>
2026-04-25 11:09:26 -0500

    tangled

--- src/sci_runner.cljs
+++ src/sci_runner.cljs
@@ -1,9 +1,28 @@
 (ns sci-runner
   (:require
    [sci.core :as sci]
+   [cljs.core.async :refer [take! chan put! close!]]
+   [cljs.js :as cljs]
+   [cljs.analyzer :as ana]
+   [cljs.env :as env]
+   [shadow.cljs.bootstrap.browser :as boot]
+   [cljs.core.async :as async]
    [re-frame.core :as rf]
    [reagent.core :as r]
+   [sci.configs.reagent.reagent :as sci-reagent]
+   [sci.configs.re-frame.re-frame :as sci-re-frame]
+   [clojure.string]
+   [cljs-workers.core :as main]
+   [sci-shared]
+   [promesa.core]
+   [input.composer]
+   [utils.logger]
+   [utils.helpers]
+   [utils.images]
+   [utils.svg]
+   [utils.macros :refer [expose-ns]]
    [client.state :as state]))
+
 
 
 
@@ -19,29 +38,52 @@
              (list 'swap! 'client.state/!active-overrides 'assoc (keyword (name comp-name))
                    (list 'hash-map :plugin-id plugin-id :fn comp-name)))))))
 
+(def log-info-macro (sci/new-macro-var 'info (fn [form _env & args] (list* 'taoensso.timbre/plugin-info @!current-eval-plugin (:line (meta form)) args))))
+(def log-warn-macro (sci/new-macro-var 'warn (fn [form _env & args] (list* 'taoensso.timbre/plugin-warn @!current-eval-plugin (:line (meta form)) args))))
+(def log-error-macro (sci/new-macro-var 'error (fn [form _env & args] (list* 'taoensso.timbre/plugin-error @!current-eval-plugin (:line (meta form)) args))))
+(def log-debug-macro (sci/new-macro-var 'debug (fn [form _env & args] (list* 'taoensso.timbre/plugin-debug @!current-eval-plugin (:line (meta form)) args))))
 
 (defn safe-reg-slot-item [slot-id item]
   (state/reg-slot-item slot-id (assoc item :plugin-id @!current-eval-plugin)))
 
+
 (def plugin-context
   (sci/init
    {:namespaces
-    {'user {'defoverride defoverride-macro}
-     're-frame.core {'dispatch rf/dispatch
-                     'subscribe rf/subscribe}
-     'reagent.core  {'as-element r/as-element}
-     'client.state  {'!components state/!components
-                     'reg-slot-item safe-reg-slot-item
-                     '!active-overrides state/!active-overrides
-                     'remove-plugin-overrides! state/remove-plugin-overrides!
-                     'get-slot state/get-slot
-                     '!config state/!config}}
-    :classes {'js goog/global}}))
+    (merge
+     sci-shared/common-namespaces
+     (:namespaces sci-reagent/config)
+     (:namespaces sci-re-frame/config)
+     {'user {'defoverride defoverride-macro}
+      'sci-runner {'!current-eval-plugin !current-eval-plugin}
 
+      'taoensso.timbre {'info  log-info-macro
+                        'warn  log-warn-macro
+                        'error log-error-macro
+                        'debug log-debug-macro
+                        'plugin-info  utils.logger/plugin-info
+                        'plugin-warn  utils.logger/plugin-warn
+                        'plugin-error utils.logger/plugin-error
+                        'plugin-debug utils.logger/plugin-debug}
+
+      'utils.macros   (expose-ns utils.macros)
+      'utils.helpers  (expose-ns utils.helpers)
+      'utils.images   (expose-ns utils.images)
+      'utils.svg      (expose-ns utils.svg)
+      'input.composer (expose-ns input.composer)
+      'client.state  {'!components state/!components
+                      'reg-slot-item safe-reg-slot-item
+                      '!active-overrides state/!active-overrides
+                      'remove-plugin-overrides! state/remove-plugin-overrides!
+                      'get-slot state/get-slot
+                      '!config state/!config
+                      '!engine-pool state/!engine-pool}
+                      
+      'cljs-workers.core {'do-with-pool! main/do-with-pool!}})
+
+    :classes sci-shared/common-classes}))
 
 (defn evaluate-ui-form [plugin-id form-str]
   (reset! !current-eval-plugin plugin-id)
   (sci/eval-string* plugin-context form-str))
-
-
 
