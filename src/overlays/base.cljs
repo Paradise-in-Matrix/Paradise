301598764915621a598f49ea801e745d7209baf2
GigiaJ <gigiaboone@yahoo.com>
2026-04-25 11:09:30 -0500

    tangled

--- src/overlays/base.cljs
+++ src/overlays/base.cljs
@@ -8,9 +8,18 @@
     [:div {:style {:padding "24px" :background "white" :color "black" :border-radius "8px"}}
      [:p modal-id]]))
 
+(defmethod modal-component :plugin-portal [_]
+  (fn [{:keys [render-fn] :as props}]
+    [render-fn props]))
+
+
 (defmulti popover-component identity)
 
 (defmethod popover-component :default [popover-id]
   (fn [_props]
     [:div {:style {:background "red" :color "white" :padding "10px"}}
       popover-id]))
+
+(defmethod popover-component :plugin-portal [_]
+  (fn [{:keys [render-fn] :as props}]
+    [render-fn props]))
