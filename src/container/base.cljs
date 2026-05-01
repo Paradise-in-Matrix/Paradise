95ad34e8b5c413802e9deece5351d097a97c4542
GigiaJ <gigiaboone@yahoo.com>
2026-05-01 13:19:41 -0500

    tangled

--- src/container/base.cljs
+++ src/container/base.cljs
@@ -13,7 +13,10 @@
    [taoensso.timbre :as log]
    [promesa.core :as p]
    [reagent.core :as r]
-   [reagent.dom.client :as rdom]))
+   [reagent.dom.client :as rdom]
+   [plugins :as plugins]
+   [utils.macros :refer [defui]]))
+
 
 (defn thread-view []
   [:div.thread-view {:style {:padding "20px" :height "100%"}}
@@ -26,63 +29,45 @@
    [:div "Thread 1..."]
   [:div "Thread 2..."]])
 
-(defn container []
-  (let [main-focus    @(re-frame/subscribe [:container/main-focus])
-        side-panel    @(re-frame/subscribe [:container/side-panel])
-        active-room   @(re-frame/subscribe [:rooms/active-id])
-        tr            @(re-frame/subscribe [:i18n/tr])]
-    [:div.room-layout
-     {:style {:display "grid"
-              :flex "1"
-              :min-width "0"
-              :grid-template-columns (if side-panel "1fr 350px" "1fr")
-              :height "100%"}}
+(defui main-content []
+  (let [active-room  @(re-frame/subscribe [:rooms/active-id])
+        main-focus   @(re-frame/subscribe [:container/main-focus])]
    [:div.main-content
-    (case main-focus
-      :call     [call-view active-room]
-      :thread   [thread-view]
-      :timeline [timeline])]
-     (when-let [panel side-panel]
+      (case main-focus
+        :call     [call-view active-room]
+        :thread   [thread-view]
+        :timeline [timeline]
+        )]))
+
+(defui side-content [side-panel]
+  (let [side-panel   @(re-frame/subscribe [:container/side-panel])
+        tr           @(re-frame/subscribe [:i18n/tr])]
+    (when-let [panel side-panel]
        [:div.sidebar
-            {:style {:display "flex"
-                     :flex-direction "column"
-                     :height "100%"
-                     :background "var(--surface-1)"
-                     :border-left "1px solid var(--border-color)"}}
-            (when (contains? #{:members :threads :search :pins :timeline} panel)
-              [:div.sidebar-header
-               {:style {:display "flex"
-                        :align-items "center"
-                        :justify-content "space-between"
-                        :padding "0 16px"
-                        :height "48px"
-                        :border-bottom "1px solid var(--border-color)"}}
-               [:h3.sidebar-title
-                {:style {:font-size "0.9rem" :text-transform "uppercase" :letter-spacing "0.05rem"}}
-                (case panel
-                  :members  (tr [:container.panels/members])
-                  :threads  (tr [:container.panels/threads])
-                  :search   (tr [:container.panels/search])
-                  :pins     (tr [:container.panels/pins])
-                  :timeline ""
-                  )]
-               [:button.sidebar-close
-                {:style {:background "transparent" :border "none" :color "var(--text-secondary)" :cursor "pointer"}
-                 :on-click #(re-frame/dispatch [:container/set-side-panel nil])}
-                [icons/exit]]])
+        (when (contains? #{:members :threads :search :pins :timeline} panel)
+          [:div.sidebar-header
+           [:h3.sidebar-title
+            (case panel
+              :members  (tr [:container.panels/members])
+              :threads  (tr [:container.panels/threads])
+              :search   (tr [:container.panels/search])
+              :pins     (tr [:container.panels/pins])
+              :timeline "")]
+           [:button.sidebar-close
+            {:on-click #(re-frame/dispatch [:container/set-side-panel nil])}
+            [icons/exit]]])
 
-            [:div.sidebar-content
-             {:style {:flex 1 :overflow "hidden"}}
-             (case panel
-               :timeline [timeline :compact? true :hide-header? true]
-               :members  [member-list]
-               :threads  [thread-list]
-               :search   [search]
-               :pins     [pins]
-               )]])
-           ]))
+        [:div.sidebar-content
+         (case panel
+           :timeline [timeline :compact? true :hide-header? true]
+           :members  [member-list]
+           :threads  [thread-list]
+           :search   [search]
+           :pins     [pins])]])))
 
-
-
-
-
+(defui container []
+  (let [side-panel   @(re-frame/subscribe [:container/side-panel])]
+    [:div.room-layout
+     {:class (when side-panel "with-sidebar")}
+     [main-content]
+     [side-content side-panel]]))
