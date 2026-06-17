66f96828c19eb1747d2a9a0f3c79f8ae69227588
GigiaJ <gigiaboone@yahoo.com>
2026-06-17 03:16:30 -0500

    tangled

--- src/paradise/shared/client/registry.cljs
+++ src/paradise/shared/client/registry.cljs
@@ -42,3 +42,29 @@
     (map dehydrate x)
 
     :else x))
+
+(defn reg-slot-item [slot-id component]
+  (swap! !slots update slot-id (fnil conj []) component))
+
+(defn get-slot [slot-id]
+  (->> (get @!slots slot-id [])
+       (sort-by #(get % :order 0))
+       (map (fn [{:keys [id component]}]
+              ^{:key (or id (random-uuid))} [component]))))
+
+(defn remove-plugin-overrides! [target-plugin-id]
+  (let [target-str (name target-plugin-id)]
+    (swap! !active-overrides
+           (fn [overrides]
+             (into {} (remove (fn [[_ override-data]]
+                                (= (name (:plugin-id override-data)) target-str))
+                              overrides))))))
+
+(defn remove-plugin-slots! [target-plugin-id]
+  (let [target-str (name target-plugin-id)]
+    (swap! !slots
+           (fn [slots]
+             (into {}
+                   (map (fn [[slot-id items]]
+                          [slot-id (into [] (remove #(= (name (:plugin-id % "unknown")) target-str) items))])
+                        slots))))))
