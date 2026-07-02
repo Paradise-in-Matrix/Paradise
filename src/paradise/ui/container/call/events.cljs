6e62795846d8e5e60c50cd100f3ab9a541d1a0b8
GigiaJ <gigiaboone@yahoo.com>
2026-07-02 11:56:11 -0500

    tangled

--- src/paradise/ui/container/call/events.cljs
+++ src/paradise/ui/container/call/events.cljs
@@ -2,58 +2,86 @@
   (:require [re-frame.core :as rf]
             [taoensso.timbre :as log]
             [cljs-workers.core :as main]
+            [clojure.string :as str]
             [paradise.shared.client.state :as state]
             [paradise.ui.container.call.call-container :refer [primary-iframe-ref backup-iframe-ref apply-iframe-sound-state!]]))
 
+(defn get-active-iframe-ref [db]
+  (let [active-iframe (get-in db [:call :active-iframe])
+        visible-iframe (get-in db [:call :visible-iframe] :primary)
+        target (or active-iframe visible-iframe)]
+    (if (= target :primary) primary-iframe-ref backup-iframe-ref)))
+
+(defn get-visible-iframe-ref [db]
+  (let [visible-iframe (get-in db [:call :visible-iframe] :primary)]
+    (if (= visible-iframe :primary) primary-iframe-ref backup-iframe-ref)))
+
 (defn click-iframe-button! [iframe-ref test-id]
   (when-let [iframe @iframe-ref]
-    (let [doc (or (.-contentDocument iframe)
-                  (.. iframe -contentWindow -document))]
+    (let [doc (or (.-contentDocument iframe) (.. iframe -contentWindow -document))]
       (if-let [btn (.querySelector doc (str "[data-testid='" test-id "']"))]
         (.click btn)
         (log/error "Could not find button with test-id:" test-id)))))
 
 (defn send-widget-action! [db action data]
-  (let [is-primary? (get-in db [:call :primary-iframe?] true)
-        iframe-ref  (if is-primary? primary-iframe-ref backup-iframe-ref)]
+  (let [iframe-ref     (get-active-iframe-ref db)
+        active-room-id (get-in db [:call :active-room-id])]
     (when-let [iframe @iframe-ref]
-      (let [widget-id  (str "element-call-" (get-in db [:call :active-room-id]))
+      (let [widget-id  (str "element-call-" active-room-id)
             req-id     (str action "-" (.now js/Date))
-            msg        #js {:api       "toWidget"
-                            :action    action
-                            :widgetId  widget-id
-                            :requestId req-id
-                            :data      (clj->js data)}]
-        (try
-          (.postMessage (.-contentWindow iframe) msg "*")
-          (catch :default e
-            (log/error "Failed to send action" action "to iframe:" e)))))))
+            msg        #js {:api "toWidget", :action action, :widgetId widget-id, :requestId req-id, :data (clj->js data)}]
+        (try (.postMessage (.-contentWindow iframe) msg "*")
+             (catch :default e (log/error "Failed to send action" action "to iframe:" e)))))))
 
 (rf/reg-event-fx
  :call/recv-widget-message
  (fn [{:keys [db]} [_ msg-string]]
-   (let [is-primary? (get-in db [:call :primary-iframe?] true)
-         iframe-ref  (if is-primary? primary-iframe-ref backup-iframe-ref)]
+   (let [parsed         (js/JSON.parse msg-string)
+         widget-id      (.-widgetId parsed)
+         target-room-id (when widget-id (str/replace widget-id #"^element-call-" ""))
+         iframes        (get-in db [:call :iframes])
+         iframe-ref     (cond
+                          (= (:primary iframes) target-room-id) primary-iframe-ref
+                          (= (:backup iframes) target-room-id)  backup-iframe-ref
+                          :else (get-visible-iframe-ref db))]
      (when-let [iframe @iframe-ref]
-       (try
-         (.postMessage (.-contentWindow iframe) (js/JSON.parse msg-string) "*")
-         (catch :default e
-           (log/error "Failed to pipe worker message to iframe:" e)))))
+       (try (.postMessage (.-contentWindow iframe) parsed "*")
+            (catch :default e (log/error "Failed to pipe worker message to iframe:" e)))))
    {}))
+
 
 (rf/reg-event-fx
  :call/hangup
  (fn [{:keys [db]} [_ opts]]
-   (log/info "Call: Sending hangup action to widget...")
-   (let [wipe?       (get opts :wipe-state? true)
-         is-primary? (get-in db [:call :primary-iframe?] true)
-         iframe-ref  (if is-primary? primary-iframe-ref backup-iframe-ref)]
-     (send-widget-action! db "im.vector.hangup" {})
-     (when-let [iframe @iframe-ref]
-       (set! (.-src iframe) "about:blank"))
+   (let [wipe?          (get opts :wipe-state? true)
+         target-room-id (or (:room-id opts) (get-in db [:call :active-room-id]))
+         iframes        (get-in db [:call :iframes])
+         iframe-key     (cond
+                          (= (:primary iframes) target-room-id) :primary
+                          (= (:backup iframes) target-room-id) :backup
+                          :else nil)
+         iframe-ref     (cond
+                          (= iframe-key :primary) primary-iframe-ref
+                          (= iframe-key :backup)  backup-iframe-ref
+                          :else nil)]
+     (when iframe-ref
+       (click-iframe-button! iframe-ref "incall_leave")
+       (when-let [iframe @iframe-ref]
+         (let [widget-id (str "element-call-" target-room-id)
+               req-id    (str "io.element.hangup-" (.now js/Date))
+               msg       #js {:api "toWidget" :action "io.element.hangup" :widgetId widget-id :requestId req-id :data #js {}}]
+           (try (.postMessage (.-contentWindow iframe) msg "*") (catch :default _)))))
      (if wipe?
-       {:dispatch [:call/teardown]}
-       {}))))
+       {:db (-> db
+                (assoc-in [:call :active-room-id] nil)
+                (assoc-in [:call :active-iframe] nil)
+                (assoc-in [:call :iframes] {:primary nil :backup nil})
+                (assoc-in [:call :is-active?] false))
+        :dispatch [:call/teardown]}
+       (let [new-db (cond-> db
+                      iframe-key (assoc-in [:call :iframes iframe-key] nil)
+                      true       (assoc-in [:call :active-room-id] nil))]
+         {:db new-db})))))
 
 (rf/reg-event-fx
  :call/toggle-audio
@@ -80,31 +108,24 @@
 (rf/reg-event-fx
  :call/toggle-deafen
  (fn [{:keys [db]} _]
-   (let [currently-deafened? (get-in db [:call :deafened?] false)
-         new-deafen-state    (not currently-deafened?)
-         is-primary?         (get-in db [:call :primary-iframe?] true)
-         iframe-ref          (if is-primary? primary-iframe-ref backup-iframe-ref)]
-     (apply-iframe-sound-state! iframe-ref (not new-deafen-state))
+   (let [new-deafen-state (not (get-in db [:call :deafened?] false))]
+     (apply-iframe-sound-state! primary-iframe-ref (not new-deafen-state))
+     (apply-iframe-sound-state! backup-iframe-ref (not new-deafen-state))
      {:db (assoc-in db [:call :deafened?] new-deafen-state)})))
+
 
 (rf/reg-event-fx
  :call/toggle-screen-share
- (fn [{:keys [db]} _]
-   (let [is-primary? (get-in db [:call :primary-iframe?] true)
-         iframe-ref  (if is-primary? primary-iframe-ref backup-iframe-ref)]
-     (click-iframe-button! iframe-ref "incall_screenshare")
-     {})))
+ (fn [{:keys [db]} _] (click-iframe-button! (get-active-iframe-ref db) "incall_screenshare") {}))
 
 (rf/reg-event-fx
  :call/open-settings
  (fn [{:keys [db]} _]
-   (let [is-primary? (get-in db [:call :primary-iframe?] true)
-         iframe-ref  (if is-primary? primary-iframe-ref backup-iframe-ref)]
-     (when-let [iframe @iframe-ref]
-       (let [doc (or (.-contentDocument iframe) (.. iframe -contentWindow -document))
-             btn (.querySelector doc "button[aria-labelledby=':rt:']")]
-         (if btn (.click btn) (log/error "Settings button not found"))))
-     {})))
+   (when-let [iframe @(get-visible-iframe-ref db)]
+     (let [doc (or (.-contentDocument iframe) (.. iframe -contentWindow -document))
+           btn (.querySelector doc "button[aria-labelledby=':rt:']")]
+       (if btn (.click btn) (log/error "Settings button not found")))) {}))
+
 
 (rf/reg-event-db
  :call/toggle-chat
@@ -114,22 +135,35 @@
 (rf/reg-event-db
  :call/toggle-iframe
  (fn [db _]
-   (update-in db [:call :primary-iframe?] not)))
+   (let [current-vis (get-in db [:call :visible-iframe] :primary)]
+     (assoc-in db [:call :visible-iframe] (if (= current-vis :primary) :backup :primary)))))
+
+(rf/reg-event-fx
+ :call/handle-widget-hangup
+ (fn [{:keys [db]} [_ widget-id]]
+   (let [target-room-id (when widget-id (str/replace widget-id #"^element-call-" ""))
+         active-room-id (get-in db [:call :active-room-id])]
+     (if (and active-room-id (= target-room-id active-room-id))
+       {:dispatch [:call/teardown]}
+       {}))))
 
 (rf/reg-event-fx
  :call/teardown
  (fn [{:keys [db]} _]
-   (log/info "Call: Tearing down persistent call layer")
    (when-let [pool @state/!engine-pool]
      (main/do-with-pool! pool {:handler :teardown-widget}))
-
-   (when-let [p-iframe @primary-iframe-ref] (set! (.-src p-iframe) "about:blank"))
-   (when-let [b-iframe @backup-iframe-ref] (set! (.-src b-iframe) "about:blank"))
+   (when-let [p-iframe @primary-iframe-ref]
+     (js/setTimeout #(set! (.-src p-iframe) "about:blank") 5000))
+   (when-let [b-iframe @backup-iframe-ref]
+     (js/setTimeout #(set! (.-src b-iframe) "about:blank") 5000))
 
    {:db (-> db
             (assoc-in [:call :active-room-id] nil)
+            (assoc-in [:call :active-iframe] nil)
+            (assoc-in [:call :iframes] {:primary nil :backup nil})
             (assoc-in [:call :is-active?] false)
             (assoc :main-focus :timeline))}))
+
 
 (rf/reg-event-db :call/set-active-room
  (fn [db [_ room-id]]
@@ -139,9 +173,11 @@
        (assoc-in [:call :audio-enabled?] true)
        (assoc-in [:call :video-enabled?] false))))
 
+
 (rf/reg-event-db :call/set-active
  (fn [db [_ active?]]
    (assoc-in db [:call :is-active?] active?)))
+
 
 (rf/reg-event-db :call/update-media-state
  (fn [db [_ {:keys [audio video]}]]
@@ -157,4 +193,4 @@
 (rf/reg-sub :call/state            (fn [db _] (:call db)))
 (rf/reg-sub :call/active-room      (fn [db _] (get-in db [:call :active-room-id])))
 (rf/reg-sub :call/is-active?       (fn [db _] (get-in db [:call :is-active?])))
-(rf/reg-sub :call/is-primary-iframe? (fn [db _] (get-in db [:call :primary-iframe?] true)))
+(rf/reg-sub :call/is-primary-iframe? (fn [db _] (= (get-in db [:call :visible-iframe] :primary) :primary)))
