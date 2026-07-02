53d88d7b63ca5f520c619bf6738c4b3d81061056
GigiaJ <gigiaboone@yahoo.com>
2026-07-01 20:37:08 -0500

    update to avoid electron sync

--- scripts/macsync.sh
+++ scripts/macsync.sh
@@ -1,4 +1,4 @@
 while true; do
-  find . -type d \( -path "./node_modules" -o -path "./.git" -o -path "./.shadow-cljs" \) -prune -o -print | \
-  entr -d rsync -avz --exclude 'node_modules' --exclude '.git' --exclude '.shadow-cljs' . jaggar@192.168.3.111:/Users/jaggar/Code/Paradise/
+    find . -type d \( -path "./node_modules" -o -path "./.git" -o -path "./.shadow-cljs" -o -path "./electron" \) -prune -o -print | \
+        entr -d rsync -avz --exclude 'node_modules' --exclude '.git' --exclude '.shadow-cljs' --exclude 'electron' . jaggar@192.168.3.111:/Users/jaggar/Code/Paradise/
 done
