8d8e867b455458ffafbc030dbc8ce297ab8a52c8
GigiaJ <gigiaboone@yahoo.com>
2026-07-02 11:56:03 -0500

    tangled

--- src/paradise/ui/container/call/call_container.cljs
+++ src/paradise/ui/container/call/call_container.cljs
@@ -4,7 +4,6 @@
             [re-frame.core :as rf]
             [re-frame.db :as rf-db]
             [paradise.shared.client.state :as state]
-            [paradise.shared.utils.macros :refer [defui]]
             ))
 
 (defonce primary-iframe-ref (r/atom nil))
@@ -30,8 +29,15 @@
                     (do
                       (let [media-observer (js/MutationObserver.
                                             (fn [_]
-                                              (let [deafened? (get-in @rf-db/app-db [:call :deafened?] false)]
-                                                (when deafened?
+                                              (let [db @rf-db/app-db
+                                                    deafened? (get-in db [:call :deafened?] false)
+                                                    active-iframe (get-in db [:call :active-iframe])
+                                                    is-primary-ref? (= iframe-ref primary-iframe-ref)
+                                                    is-active? (if active-iframe
+                                                                 (= active-iframe (if is-primary-ref? :primary :backup))
+                                                                 (= (get-in db [:call :visible-iframe] :primary) (if is-primary-ref? :primary :backup)))]
+                                                (if is-active?
+                                                  (apply-iframe-sound-state! iframe-ref (not deafened?))
                                                   (apply-iframe-sound-state! iframe-ref false)))))]
                         (.observe media-observer (.-body doc) #js {:childList true :subtree true}))
                       (if-let [share-btn (.querySelector doc "[data-testid='incall_screenshare']")]
@@ -46,29 +52,33 @@
                   (js/setTimeout check-dom 400)))))]
     (check-dom)))
 
-(defui persistent-call-container []
+(defn ^:ui persistent-call-container []
   (let [call-state   @(rf/subscribe [:call/state])
         tr           @(rf/subscribe [:i18n/tr])
-        primary?     (get call-state :primary-iframe? true)
+        visible      (get call-state :visible-iframe :primary)
+        primary-vis? (= visible :primary)
         rect         @host-rect
-        active-style (if rect
-                       {:position "fixed" :top (str (:top rect) "px") :left (str (:left rect) "px")
-                        :width (str (:width rect) "px") :height (str (:height rect) "px")
-                        :border "none" :pointer-events "auto" :display "block"}
-                       {:border "none" :display "none"})
-        hidden-style {:border "none" :display "none"}]
+        offscreen-style {:position "absolute" :top "-9999px" :left "-9999px"
+                         :width "1px" :height "1px" :border "none"
+                         :opacity 0 :pointer-events "none" :z-index -1}
+        active-style    (if rect
+                          {:position "fixed" :top (str (:top rect) "px") :left (str (:left rect) "px")
+                           :width (str (:width rect) "px") :height (str (:height rect) "px")
+                           :border "none" :pointer-events "auto" :display "block"}
+                          offscreen-style)
+        hidden-style    offscreen-style]
     [:div.persistent-call-layer
      {:style {:position "fixed" :top 0 :left 0 :width "100%" :height "100%"
               :pointer-events "none" :z-index 99
               :display (if (and (:mobile? call-state) (:chat-open? call-state)) "none" "block")}}
      [:iframe {:ref #(reset! primary-iframe-ref %) :title (tr [:container.calls/main-iframe])
-               :style (if primary? active-style hidden-style)
+               :style (if primary-vis? active-style hidden-style)
                :sandbox "allow-forms allow-scripts allow-same-origin allow-popups allow-modals allow-downloads"
                :allow "camera; microphone; display-capture; autoplay; encrypted-media; fullscreen;"
                :on-load #(attach-iframe-observers! primary-iframe-ref)
                :src "about:blank"}]
      [:iframe {:ref #(reset! backup-iframe-ref %) :title (tr [:container.calls/backup-iframe])
-               :style (if-not primary? active-style hidden-style)
+               :style (if-not primary-vis? active-style hidden-style)
                :sandbox "allow-forms allow-scripts allow-same-origin allow-popups allow-modals allow-downloads"
                :allow "camera; microphone; display-capture; autoplay; encrypted-media; fullscreen;"
                :on-load #(attach-iframe-observers! backup-iframe-ref)
